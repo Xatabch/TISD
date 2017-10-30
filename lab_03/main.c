ce81ab954a604591ec6f5f8d3f68dad1cf65da11
Ivan Morozov <ivan.morozov@list.ru>
2017-10-30 23:18:00 +0300

    add lab_03

--- lab_03/main.c
+++ lab_03/main.c
@@ -73,79 +73,13 @@ void enter(char *str)
 //РЕАЛИЗАЦИЯ СТЕКА ОДНОСВЯЗНЫМ СПИСКОМ
 //===================================================================
 
-//ПРОВЕРКА НА КОРРЕКТНОСТЬ РАССТАВЛЕННЫХ СКОБОК В СПИСКЕ
-int proverca_list(struct skobka *head,char *str)
+//ДОБАВЛЕНИЕ ЭЛЕМЕНТА В СТЕК
+void push_list(char str,struct skobka **head)
 {
-  int i = strlen(str);
-
-  struct skobka *tmp;
-
-
-  while(i>=0)
-  {
-
-    if(str[i] == ')' || str[i] == ']' || str[i] == '}')
-    {
-      printf("\n\n%c\n\n", head->skobka);
-      if(str[i] == ')' && (head->skobka + 1) == str[i])
-      {
-        printf("SKOBKA: %c\n", (head->skobka + 1));
-        tmp = pop_list(&head);
-        free(tmp);
-      }
-      if(str[i] == ']' && (head->skobka + 2) == str[i])
-      {
-        printf("SKOBKA: %c\n", (head->skobka + 2));
-        tmp = pop_list(&head);
-        free(tmp);
-      }
-      if(str[i] == '}')
-      {
-        if((head->skobka+2) == str[i])
-        {
-          printf("SKOBKA: %c\n", (head->skobka + 2));
-          tmp = pop_list(&head);
-          free(tmp);
-        }
-      }
-    }
-    i--;
-  }
-
-  return 0;
-}
-
-//ЗАПОЛНЕНИЕ СТЕКА, РЕАЛИЗУЕМОГО СПИСКОМ
-struct skobka* get_char_list(char *str,int *n)
-{
-  int i = 0;
-
-  struct skobka *head = NULL;
   struct skobka *node = NULL;
 
-  int check_open = 0;
-
-	while(str[i] != '\0')
-	{
-		if(str[i] == '(' || str[i] == '[' || str[i] == '{')
-		{
-      check_open = 1;
-      node = create_skobka(str[i]);
-      head = add_front(head, node);
-      *n = *n + 1;
-    }
-    if(str[i] == ')' || str[i] == ']' || str[i] == '}')
-    {
-      if(check_open)
-      {
-        
-      }
-      if(!check_open)
-        head = NULL;
-    }
-    i++;
-  }
-  return head;
+  node = create_skobka(str);
+  *head = add_front(*head, node);
 }
 
 //УДАЛЕНИЕ ИЗ СТЕКА, РЕАЛИЗУЕМОГО СПИСКОМ
@@ -160,6 +94,73 @@ struct skobka *pop_list(struct skobka **head)
     return out;
 }
 
+//ПРОВЕРКА НА КОРРЕКТНОСТЬ РАССТАВЛЕННЫХ СКОБОК В СПИСКЕ
+void proverca_list(int i, struct skobka *head, char *str,int *count)
+{
+  struct skobka *tmp;
+
+  while(str[i] != '\0')
+  {
+    if (str[i] == '(' || str[i] == '[' || str[i] == '{')
+      break;
+
+    if(str[i] == ')' || str[i] == ']' || str[i] == '}')
+    {
+      if(str[i] == ')' && (head->skobka + 1) == str[i])
+      {
+        *count = *count + 1;
+        tmp = pop_list(&head);
+        free(tmp);
+      }
+      if(str[i] == ']' && (head->skobka + 2) == str[i])
+      {
+        *count = *count + 1;
+        tmp = pop_list(&head);
+        free(tmp);
+      }
+      if(str[i] == '}')
+      {
+        if((head->skobka+2) == str[i])
+        {
+          *count = *count + 1;
+          tmp = pop_list(&head);
+          free(tmp);
+        }
+      }
+    }
+  i++;
+  }
+}
+
+//ЗАПОЛНЕНИЕ СТЕКА, РЕАЛИЗУЕМОГО СПИСКОМ
+struct skobka* get_char_list(char *str,int *n,int *count)
+{
+  int i = 0;
+
+  int check_open = 0;
+
+  struct skobka *head = NULL;
+
+	while(str[i] != '\0')
+	{
+		if(str[i] == '(' || str[i] == '[' || str[i] == '{')
+		{
+      check_open = 1;
+      push_list(str[i],&head);
+      *n = *n + 1;
+    }
+    if(str[i] == ')' || str[i] == ']' || str[i] == '}')
+    {
+      if(check_open)
+        proverca_list(i, head, str, count);
+      if(!check_open)
+        head = NULL;
+    }
+    i++;
+  }
+  return head;
+}
+
 //======================================================================
 
 int main(void)
@@ -167,17 +168,18 @@ int main(void)
   struct skobka *head = NULL;
   char str[128];
   int n = 0;
-  int error;
+  int count = 0;
+  //int error;
 
 	printf("Введите выражние: ");
   enter(str);
-  printf("%s\n", str);
-  head = get_char_list(str,&n);
+  head = get_char_list(str,&n,&count);
   if(head)
   {
-    apply(head,print_person,"%c\n");
-    error = proverca_list(head,str);
-    printf("%d\n",error);
+    if(n != count)
+      printf("Вы допустили ошибку в расстановке скобок\n");
+    else
+      printf("Скобки расставлены верно\n");
   }
   else
     printf("ERRRRORRR");
